dead0e519c04ccd0f5f5473071b8c451bfe9de0c
kmdrGroch <leoniak.bartosz@gmail.com>
2020-06-24 16:38:02 +0200

    test

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ cmake -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
 make test
 make Bigint_coverage
 cd ..
-coveralls --root . -E ".*gtest.*" -E ".*CMakeFiles.*"
+cd ${TRAVIS_BUILD_DIR}
+lcov --directory . --capture --output-file coverage.info # capture coverage info
+lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
+lcov --list coverage.info #debug info
+# Uploading report to CodeCov
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
