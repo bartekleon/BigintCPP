2982c46f301c96bf0b255aff3db4924a74513672
kmdrGroch <leoniak.bartosz@gmail.com>
2020-06-24 16:12:07 +0200

    use coveralls

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cmake -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
 make test
 make Bigint_coverage
 cd ..
-codecov -t 8d1c8c84-247f-4c93-b694-77494affac62 -X gcov
+coveralls --root . -E ".*gtest.*" -E ".*CMakeFiles.*"
