a8222a2441d4eb7e2ac8320edc2fa627be7ce84a
kmdrGroch <leoniak.bartosz@gmail.com>
2020-06-26 20:16:36 +0200

    test

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ mkdir -p build
 cd build
 cmake -Wall -pedantic -Werror -Wno-variadic-macros -Wno-long-long -Wno-shadow -DCMAKE_BUILD_TYPE=Coverage -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
 make Bigint_coverage
-ctest
 codecov -t 8d1c8c84-247f-4c93-b694-77494affac62 -X gcov
