49bc4e9e8a28597503a1c974a058473ec232101e
kmdrGroch <leoniak.bartosz@gmail.com>
2020-06-27 01:37:47 +0200

    try making build fail

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ mkdir -p build
 cd build
 cmake -DCMAKE_CXX_FLAGS="-Werror" -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTS=ON ..
 make Bigint_coverage
-codecov -t 8d1c8c84-247f-4c93-b694-77494affac62 -X gcov
