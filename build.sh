e302e91e47efda1b27532b40937d6f5855f661c8
kmdrGroch <leoniak.bartosz@gmail.com>
2020-06-26 20:43:59 +0200

    test

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ rm -r build
 mkdir -p build
 
 cd build
-cmake -DCMAKE_CXX_FLAGS="-Wall -pedantic -Werror -Wno-variadic-macros -Wno-long-long -Wno-shadow" -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTS=ON ..
+cmake -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTS=ON ..
 make Bigint_coverage
 codecov -t 8d1c8c84-247f-4c93-b694-77494affac62 -X gcov
