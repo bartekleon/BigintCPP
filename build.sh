8557dd8bc32a890cd54a5afbcd8a2c1609263c60
kmdrGroch <leoniak.bartosz@gmail.com>
2020-06-26 20:40:29 +0200

    fix cmake build

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ rm -r build
 mkdir -p build
 
 cd build
-cmake -Wall -pedantic -Werror -Wno-variadic-macros -Wno-long-long -Wno-shadow -DCMAKE_BUILD_TYPE=Coverage -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
+cmake -DCMAKE_CXX_FLAGS="-Wall -pedantic -Werror -Wno-variadic-macros -Wno-long-long -Wno-shadow" -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTS=ON ..
 make Bigint_coverage
 codecov -t 8d1c8c84-247f-4c93-b694-77494affac62 -X gcov
