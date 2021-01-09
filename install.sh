0d300568cbf8f334c3064738cffe9103a623d505
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2021-01-09 23:21:40 +0100

    test

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ pip install codecov
 sudo apt-get install -y lcov
 sudo apt-get install -y clang-tidy-10
 sudo apt-get install -y clang-format-10
+
+clang --version
+clang-tidy --version
+clang-format --version
