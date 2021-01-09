bc52747df7438b2ccd5b0e555170d6b0c84b945c
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2021-01-09 23:12:38 +0100

    test

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 sudo apt-get update -y
 
-chmod +x llvm.sh
-sudo ./llvm.sh
-
 pip install codecov
 sudo apt-get install -y lcov
-sudo apt-get install -y clang-tools
+sudo apt-get install -y llvm-toolchain-10
