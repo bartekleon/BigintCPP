9cc8f28d3f3f17b07ae3a4a5a715e016f8803c5f
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2021-01-09 23:17:08 +0100

    test

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ sudo apt-get update -y
 
 pip install codecov
 sudo apt-get install -y lcov
-sudo apt-get install -y llvm-toolchain-10
+sudo apt-get install -y clang-tidy-10
+sudo apt-get install -y clang-format-10
