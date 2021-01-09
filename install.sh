8b13942256c9ef1752aa4ac494751fdec6275eb9
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2021-01-09 23:25:50 +0100

    test

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ sudo apt-get install -y lcov
 sudo apt-get install -y clang-tidy-10
 sudo apt-get install -y clang-format-10
 
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 100
+sudo update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-10 100
+sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-10 100
+
 clang --version
 clang-tidy --version
 clang-format --version
