85873d35bd639fd5791813ad8a214a9a26b40ffe
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2021-01-10 11:28:04 +0100

    feat: performance update, smallvector class

--- include/bigint.h
+++ include/bigint.h
@@ -4,100 +4,105 @@
 #define BIGINT_H_
 
 #include <algorithm>
+#include <array>
 #include <cmath>
 #include <exception>
 #include <map>
 #include <sstream>
 #include <string>
 #include <string_view>
-#include <vector>
+
+#include "smallvector.h"
+
+namespace BigMath {
 
 class Bigint {
-
 private:
-	std::vector<int> number;
-	mutable bool positive = true;
+  SmallVector<int32_t> number;// max size of vector uint32_t
+  mutable bool positive = true;
 
 public:
-	// Constructors
-	Bigint() = default;
-	virtual ~Bigint() = default;
-	Bigint(const Bigint&) = default;
-	Bigint(Bigint&&) = default;
+  // Constructors
+  Bigint() = default;
+  ~Bigint() = default;
+  Bigint(const Bigint &bigint) = default;
+  Bigint(Bigint &&bigint) = default;
 
-	Bigint(long long);
-	Bigint(std::string_view);
+  Bigint(int64_t value);
+  Bigint(std::string_view string);
 
-	// Adding
-	Bigint operator+(Bigint const&) const;
-	Bigint& operator+=(Bigint const&);
-	Bigint operator+(long long const&) const;
-	Bigint& operator+=(long long);
+  // Adding
+  Bigint operator+(Bigint const &right) const;
+  Bigint &operator+=(Bigint const &right);
+  Bigint operator+(int64_t const &value) const;
+  Bigint &operator+=(int64_t value);
 
-	// Subtraction
-	Bigint operator-(Bigint const&) const;
-	Bigint& operator-=(Bigint const&);
-	Bigint operator-(long long const&) const;
-	Bigint& operator-=(long long);
+  // Subtraction
+  Bigint operator-(Bigint const &right) const;
+  Bigint &operator-=(Bigint const &right);
+  Bigint operator-(int64_t const &value) const;
+  Bigint &operator-=(int64_t value);
 
-	// Unary minus
-	Bigint operator-();
+  // Unary minus
+  Bigint operator-();
 
-	// Multiplication
-	Bigint operator*(Bigint const&) const;
-	Bigint& operator*=(Bigint const&);
-	Bigint operator*(long long) const;
-	Bigint& operator*=(long long const&);
+  // Multiplication
+  Bigint operator*(Bigint const &right) const;
+  Bigint &operator*=(Bigint const &right);
+  Bigint operator*(int64_t value) const;
+  Bigint &operator*=(int64_t const &value);
 
-	// Division
-	Bigint operator/(Bigint q);
-	Bigint& operator/=(Bigint const&);
+  // Division
+  Bigint operator/(Bigint right);
+  Bigint &operator/=(Bigint const &right);
 
-	// Modulo
-	long long operator%(long long const&);
+  // Modulo
+  [[nodiscard]] int64_t operator%(int64_t const &value) const;
 
-	// Comparison
-	bool operator<(const Bigint&) const;
-	bool operator>(const Bigint&) const;
-	bool operator<=(const Bigint&) const;
-	bool operator>=(const Bigint&) const;
-	bool operator==(const Bigint&) const;
-	bool operator!=(const Bigint&) const;
+  // Comparison
+  [[nodiscard]] bool operator<(const Bigint &other) const;
+  [[nodiscard]] bool operator>(const Bigint &other) const;
+  [[nodiscard]] bool operator<=(const Bigint &other) const;
+  [[nodiscard]] bool operator>=(const Bigint &other) const;
+  [[nodiscard]] bool operator==(const Bigint &other) const;
+  [[nodiscard]] bool operator!=(const Bigint &other) const;
 
-	// Allocation
-	Bigint& operator=(const std::string&);
-	Bigint& operator=(const Bigint&) = default;
-	Bigint& operator=(Bigint&&) = default;
+  // Allocation
+  Bigint &operator=(const std::string &str);
+  Bigint &operator=(const Bigint &other) = default;
+  Bigint &operator=(Bigint &&other) = default;
 
-	// Access
-	int operator[](int const&);
+  // Access
+  [[nodiscard]] int32_t operator[](int32_t const &which) const;
 
-	// Input & Output
-	friend std::istream& operator>>(std::istream&, Bigint&);
-	friend std::ostream& operator<<(std::ostream&, Bigint const&);
+  // Input & Output
+  friend std::istream &operator>>(std::istream &stream, Bigint &bigint);
+  friend std::ostream &operator<<(std::ostream &stream, Bigint const &bigint);
 
-	// Helpers
-	void clear();
-	Bigint& abs();
-	std::string toString() const;
-	int digits() const;
-	bool isEven();
-	bool isNegative() const;
-	Bigint clone();
-	void flipPositive() const;
+  // Helpers
+  void clear();
+  Bigint &abs();
+  [[nodiscard]] std::string to_string() const;
+  [[nodiscard]] int32_t digits() const;
+  [[nodiscard]] bool is_even() const;
+  [[nodiscard]] bool is_negative() const;
+  Bigint clone();
+  constexpr void flip_sign() const;
 
-	Bigint& addZeroes(int);
+  Bigint &add_zeroes(uint32_t amount);
 
-	// Power
-	Bigint& pow(int const&);
+  // Power
+  Bigint &pow(uint32_t const &power);
 
 private:
-	static int segmentLength(int);
-	Bigint pow(int const&, std::map<int, Bigint>&);
-	int compare(Bigint const&) const; // 0 a == b, -1 a < b, 1 a > b
-	static Bigint getFragment(Bigint&, int);
+  [[nodiscard]] constexpr static int32_t segment_length(int32_t segment);
+  [[nodiscard]] Bigint pow(uint32_t const &power, std::map<int32_t, Bigint> *lookup);
+  [[nodiscard]] int8_t compare(Bigint const &right) const;// 0 a == b, -1 a < b, 1 a > b
+  [[nodiscard]] static Bigint get_fragment(const Bigint &bigint, int32_t which);
 };
 
-std::string toString(Bigint const&);
+std::string to_string(Bigint const &bigint);
+
+}// namespace BigMath
 
 #endif /* BIGINT_H_ */
