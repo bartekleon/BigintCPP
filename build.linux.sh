7ba91afbed9210023acdc36c64b7a91fb97de91b
Bartosz Leoniak <leoniak.bartosz@gmail.com>
2021-01-11 14:47:22 +0100

    fix: CI

--- build.linux.sh
+++ build.linux.sh
@@ -1,7 +1,17 @@
 mkdir -p build
 
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_CXX_FLAGS="-Werror" -DRUN_CLANG_TOOLS=ON -DBUILD_TESTS=OFF ..
-make
-make clang-tidy
-make clang-format
+
+exit_if_error() {
+  local exit_code=$1
+  shift
+  [[ $exit_code ]] &&
+    ((exit_code != 0)) && {
+      exit "$exit_code"
+    }
+}
+
+cmake -G "Unix Makefiles" -DCMAKE_CXX_FLAGS="-Werror" -DRUN_CLANG_TOOLS=ON -DBUILD_TESTS=OFF .. || exit_if_error
+make || exit_if_error
+make clang-tidy || exit_if_error
+make clang-format || exit_if_error
